3fafda32b967a92e67dba443f0aa823f58871968
bella <bella@bellas.cloud>
2026-09-16 18:08:49 -0400

    draft

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ky42c-root-utility
 
+Run `Unblock-File ky42c-utility.ps1` in an Administrator PowerShell to allow this script to work.
+Next, run `./ky42c-utility.ps1 -setup -unlock`. Power off your phone, and plug it in. Be sure you have the drivers.
+Then, run `./ky42c-utility.ps1 -backup`. Put the boot.img file in the backups directory through magisk on the phone,
+Use `adb reboot bootloader` then `fastboot flash boot PATH/TO/BOOT.IMG`.
+Finally, reboot the phone with `fastboot reboot`.
---
+++ ky42c-utility.ps1
@@ -0,0 +1,42 @@
+param(
+    [switch]$setup = $false,
+    [switch]$unlock = $false,
+    [switch]$backup = $false,
+    [switch]$cleanup = $false
+)
+
+if ($setup) {
+    # make venv
+    Set-Location kyocera-ky-42c-unlock
+    python -m venv venv
+    ./venv/Scripts/activate
+    pip install -r requirements.txt
+    # patch with momo's utils.py
+    Move-Item ./lib/utils.py ./lib/utils.old
+    Copy-Item ../momo-utils-py/utils.py ./lib/utils.py
+    Set-Location ..
+    # download penumbra/antumbra
+    Invoke-WebRequest https://github.com/shomykohai/penumbra/releases/download/v1.1.0/antumbra.exe -OutFile ./antumbra.exe
+    # download MTK DA from mtkclient repo
+    Invoke-WebRequest https://github.com/bkerler/mtkclient/raw/refs/heads/main/mtkclient/Loader/MTK_DA_V5.bin -OutFile ./MTK_DA_V5.bin
+}
+
+if ($unlock) {
+    Write-Output "Power off your phone and plug it in. It should reboot with 'Orange State' message."
+    python kyocera-ky-42c-unlock/main.py unlock
+}
+
+if ($backup) {
+    New-Item "./backup" -Type Directory
+    python kyocera-ky-42c-unlock/main.py patch
+    ./antumbra.exe rl backup --skip userdata --da ./MTK_DA_V5.bin
+}
+
+if ($cleanup) {
+    # undo filesystem changes, except for the backup
+    # for debugging purposes
+    Remove-Item ./antumbra.exe
+    Remove-Item ./MTK_DA_V5.bin
+    Remove-Item kyocera-ky-42c-unlock/lib/utils.py
+    Move-Item kyocera-ky-42c-unlock/lib/utils.old kyocera-ky-42c-unlock/lib/utils.py
+}
